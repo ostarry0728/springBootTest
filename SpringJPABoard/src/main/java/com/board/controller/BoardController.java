c663278349fa9cb673cfa4075a45924aa6478f1b
Oh SeungTaek <ostarry@gmail.com>
2025-01-10 17:19:48 +0900

    스프링부트 JPA  게시판 제작 테스트

---
+++ SpringJPABoard/src/main/java/com/board/controller/BoardController.java
@@ -0,0 +1,64 @@
+package com.board.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.board.domain.Board;
+import com.board.service.BoardService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+	@Autowired
+	public BoardService service;
+
+	@RequestMapping(value = "/registerForm", method = RequestMethod.GET)
+	public String registerForm(Board board, Model model) throws Exception {
+		log.info("registerForm");
+		return "board/register";
+	}
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String register(Board board, Model model) throws Exception {
+		log.info("register");
+		service.register(board);
+		model.addAttribute("msg", "등록이 완료되었습니다.");
+		return "board/success";
+	}
+
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public void list(Model model) throws Exception {
+		log.info("list");
+		model.addAttribute("list", service.list());
+	}
+
+	@RequestMapping(value = "/read", method = RequestMethod.GET)
+	public void read(Board board, Model model) throws Exception {
+		log.info("read");
+		model.addAttribute(service.read(board.getBoardNo()));
+	}
+
+	@RequestMapping(value = "/remove", method = RequestMethod.GET)
+	public String remove(Board board, Model model) throws Exception {
+		log.info("remove");
+		service.remove(board.getBoardNo());
+		model.addAttribute("msg", "삭제에 성공하였습니다.");
+		return "board/success";
+	}
+
+	@RequestMapping(value = "/modify", method = RequestMethod.POST)
+	public String modify(Board board, Model model) throws Exception {
+		log.info("modify");
+		service.modify(board);
+		model.addAttribute("msg", "수정이 완료되었습니다.");
+		return "board/success";
+	}
+
+}
---
+++ SpringJPABoard/src/main/java/com/board/domain/Board.java
@@ -0,0 +1,38 @@
+package com.board.domain;
+
+import java.util.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@SequenceGenerator(name = "JPABOARD_SEQ_CEN", sequenceName = "JPABOARD_SEQ", initialValue = 1, allocationSize = 1)
+@Table(name = "jpaboard")
+public class Board {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "JPABOARD_SEQ_CEN")
+	@Column(name = "board_no")
+	private long boardNo;
+
+	@Column(name = "title")
+	private String title;
+
+	@Column(name = "content")
+	private String content;
+
+	@Column(name = "writer")
+	private String writer;
+
+	@CreationTimestamp
+	@Column(name = "reg_date")
+	private Date regDate;
+}
